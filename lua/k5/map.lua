c444e925ef2f4455b7bd01f632ba80f578af786e
K5 Asus <jacouton.valentin@gmail.com>
2025-02-23 17:44:47 +0100

    add greatest remap ever

--- lua/k5/map.lua
+++ lua/k5/map.lua
@@ -5,3 +5,13 @@ vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
 
 vim.keymap.set('n', '<leader>sv', vim.cmd.Ex)
+
+-- NOTE: JV11 "Greatest remap ever"
+
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
+
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
